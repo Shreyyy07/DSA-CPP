dbb8fe63e0918f9609cc80d91628f073bed40be1
shrey joshi <113532718+Shreyyy07@users.noreply.github.com>
2024-01-18 00:44:42 +0530

    Time: 5 ms (57.81%) | Memory: 15.2 MB (25.65%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -10,55 +10,41 @@
  */
 class Solution {
 public:
-// ListNode* solve(ListNode* list1, ListNode* list2){
-//     if(list1->next==NULL){
-//         list1->next=list2;
-//         return list1;
-//     }
-//     ListNode* curr1=list1;
-//     ListNode* next1=curr1->next;
-//     ListNode* curr2=list2;
-//     ListNode* next2=curr2->next;    
-
-//     while(next1!=NULL && curr2!=NULL){
-//         if(curr2->val>=curr1->val && 
-//            curr2->val<=next1->val){
-//                curr1->next=curr2;
-//                next2=curr2->next;
-//                curr2->next=next1;
-
-//                curr1=curr2;
-//                curr2=next1;
-//            }
-//            else{
-//                curr1=next1;
-//                next1=next1->next;
-
-//                if(next1==NULL){
-//                    curr1->next=curr2;
-//                    return list1;
-//                }
-//            }
-//     }
-//     return list1;
-
-// }
-
-
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL){
+        if(list1==NULL)
             return list2;
-        }
-        if(list2==NULL){
+        
+        if(list2==NULL)
             return list1;
-        }
+        
+        ListNode* ans=new ListNode(-1);
+        ListNode* temp=ans;
+
+        while(list1!=NULL && list2!=NULL){
+
         if(list1->val<=list2->val){
-            list1->next=mergeTwoLists(list1->next,list2);
-            return list1;
+            temp->next=list1;
+            temp=list1;
+            list1=list1->next;
         }
         else{
-         list2->next= mergeTwoLists(list1,list2->next);
-         return list2;
+          temp->next=list2;
+            temp=list2;
+            list2=list2->next;
         }
-}
+        }
+
+        while(list1!=NULL){
+            temp->next=list1;
+            temp=list1;
+            list1=list1->next;
+        }
+
+        while(list2!=NULL){
+            temp->next=list2;
+            temp=list2;
+            list2=list2->next;
+        }
+    return ans->next;
+    }
 };
