8517d4e12d137b16e999fdc70a80e5da63c531c7
shrey joshi <113532718+Shreyyy07@users.noreply.github.com>
2024-03-04 22:42:49 +0530

    Time: 11 ms (33.33%) | Memory: 24 MB (66.67%) - LeetSync

---
+++ 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int count=0;
+        vector<int>ans;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]<k){
+                ans.push_back(nums[i]);
+                count++;
+            }
+           
+        }
+         return count;
+        
+    }
+};
