9e0583178ab8b25eb1e5a792878740c1b670130e
shrey joshi <113532718+Shreyyy07@users.noreply.github.com>
2024-04-21 17:45:17 +0530

    Strings day1

--- Linked list/141-linked-list-cycle/linked-list-cycle.cpp
+++ Linked list/141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,20 +9,24 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+      
+
         if(head==NULL){
             return false;
         }
-        ListNode* slow=head;
-        ListNode* fast=head->next;
+        ListNode *slow=head;
+        ListNode *fast=head->next;
 
         while(fast!=NULL && fast->next!=NULL){
-             if(slow==fast){
-            return true;
-        }
+            if(slow==fast){
+                return true;
+            }
+            else{
             slow=slow->next;
             fast=fast->next->next;
-        }
-        // return slow;
-     return false;  
     }
+        }
+     return false;
+    }
+   
 };
