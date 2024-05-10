571c9fa65aebce29a056b23f7b5f87a6016b92d1
shrey joshi <113532718+Shreyyy07@users.noreply.github.com>
2024-05-10 22:33:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (64.98%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -0,0 +1,22 @@
+#include <algorithm> 
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int n=word.size();
+        int index=-1;
+
+        for(int i=0; i<n; i++){
+           if(word[i]==ch){
+            index=i;
+            break;
+           }
+        }
+           if(index==-1){
+            return word;
+           }
+
+        
+           reverse(word.begin(),word.begin()+index+1);
+           return word;
+    }
+};
