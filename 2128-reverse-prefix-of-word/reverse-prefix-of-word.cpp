4dc25520d07bc8909874c31ac446e8071602215f
shrey joshi <113532718+Shreyyy07@users.noreply.github.com>
2024-05-11 21:14:11 +0530

    Time: 3 ms (41.92%) | Memory: 7.4 MB (65.01%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -1,22 +1,18 @@
-#include <algorithm> 
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
+
+        int index=0;
         int n=word.size();
-        int index=-1;
 
         for(int i=0; i<n; i++){
-           if(word[i]==ch){
-            index=i;
-            break;
-           }
+            if(word[i]==ch){
+                index=i;
+                break;
+            }
         }
-           if(index==-1){
-            return word;
-           }
-
+        reverse(word.begin(),word.begin()+index+1);
+        return word;
         
-           reverse(word.begin(),word.begin()+index+1);
-           return word;
     }
 };
