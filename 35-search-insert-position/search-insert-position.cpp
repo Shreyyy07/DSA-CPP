d1d0ead756f3a3f111bd4b910f4ae399c67fd26f
shrey joshi <113532718+Shreyyy07@users.noreply.github.com>
2024-06-21 17:18:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (7.82%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -4,21 +4,18 @@ public:
         
         int s=0; 
         int e=nums.size()-1;
-        vector<int>ans;
+        int ans=nums.size();
 
         while(s<=e){
             int mid=s+(e-s)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]>=target){
-                e=mid-1;
-            }
-            else if(nums[mid]<target){
-                s=mid+1;
-            }
-           
+        if(nums[mid]>=target){
+            ans=mid;
+            e=mid-1;
         }
-        return s;
+        else{
+            s=mid+1;
+        } 
+        }
+        return ans;
     }
 };
