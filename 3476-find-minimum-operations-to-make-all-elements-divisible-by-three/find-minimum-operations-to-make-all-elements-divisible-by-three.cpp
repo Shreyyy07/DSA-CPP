4fa098ec3dd10227c9b7d5efcdcaabe64a5851dd
shrey joshi <113532718+Shreyyy07@users.noreply.github.com>
2024-06-25 14:22:35 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (63.30%) - LeetSync

--- 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -5,15 +5,15 @@ public:
 
        
         for(int i=0; i<nums.size(); i++){
-             if(nums[i]==1){
-            nums[i]=nums[i]-1;
-            count++;
-        }
-        else if(nums[i]==2){
-            nums[i]=nums[i]+1;
-            count++;
-        }
-            else if(nums[i]%3!=0){
+        //      if(nums[i]==1){
+        //     nums[i]=nums[i]-1;
+        //     count++;
+        // }
+        // else if(nums[i]==2){
+        //     nums[i]=nums[i]+1;
+        //     count++;
+        // }
+             if(nums[i]%3!=0){
                 count++;
             }
         }
