dc1ba4a02c240cf05928622bcc7f62474c090ada
shrey joshi <113532718+Shreyyy07@users.noreply.github.com>
2024-07-02 16:15:10 +0530

    Time: 6 ms (32.80%) | Memory: 14.3 MB (7.47%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
 
+        unordered_map<int,int>count;
+        // created a hashmap to store the count of the elements
         vector<int>ans;
-        int i=0,j=0;
 
-              while(i<nums1.size() && j<nums2.size()){
-                if(nums1[i]==nums2[j]){
-                    ans.push_back(nums1[i]);
-                    i++;
-                    j++;
-                }
-                else if(nums1[i]<=nums2[j]){
-                    i++;
-                }
-                else if(nums1[i]>=nums2[j]){
-                    j++;
-                }
+        for(int i:nums1){
+            //traverse through the elements and count the occ of each element
+            count[i]++;
+        }
+        for(int i:nums2){
+            //now check if the ele of 1st array is found in second , also check for its occ
+            if(count[i]>0){
+                ans.push_back(i);
+                // if the occ is greater than 0 than push the ele 
+                count[i]--;
+                // after pushing the ele itself decrement the count
             }
+        }
         return ans;
-        
     }
 };
