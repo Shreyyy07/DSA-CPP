6136e55319f09af6b346508d6a3e2781664db4aa
shrey joshi <113532718+Shreyyy07@users.noreply.github.com>
2024-07-04 00:54:01 +0530

    Time: 2 ms (42.80%) | Memory: 9.9 MB (17.53%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -1,30 +1,54 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-
-void preOrder(TreeNode* root,vector<int>&ans){
-
-     if(root == NULL){
-            return ;
-        }
-       ans.push_back(root->val);
-       preOrder(root->left,ans);
-       preOrder(root->right,ans);
-
-}
+    // Function to perform preorder traversal
+    // of a binary tree iteratively
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>ans1;
-        preOrder(root,ans1);
-        return ans1;
+        // Initialize vector to store
+        // the preorder traversal result
+        vector<int> preorder;
+        
+        // If the root is null, return
+        // an empty traversal result
+        if(root == nullptr) {
+            return preorder;
+        }
+        
+        // Create a stack to store
+        // nodes during traversal
+        stack<TreeNode*> st;
+        // Push the root node
+        // onto the stack
+        st.push(root);
+        
+        // Perform iterative preorder traversal
+        while(!st.empty()) {
+            // Get the current node
+            // from the top of the stack
+            root = st.top();
+            // Remove the node
+            // from the stack
+            st.pop();
+            
+            // Add the node's value to
+            // the preorder traversal result
+            preorder.push_back(root->val);
+            
+            // Push the right child
+            // onto the stack if exists
+            if(root->right != nullptr) {
+                st.push(root->right);
+            }
+            
+            // Push the left child onto
+            // the stack if exists
+            if(root->left != nullptr) {
+                st.push(root->left);
+            }
+        }
+        
+        // Return the preorder
+        // traversal result
+        return preorder;
     }
 };
