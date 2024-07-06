2e715a96de4f82579e71c3d80c9ac0bfe6eef2d7
shrey joshi <113532718+Shreyyy07@users.noreply.github.com>
2024-07-06 23:35:35 +0530

    Time: 2 ms (54.14%) | Memory: 7.2 MB (30.58%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        int div=time/(n-1);
-        int rem=time%(n-1);
 
-        if(div%2==1){
+        int div=time/(n-1);
+        // this will tell the direction 
+        // if dir is 1 = now we will moving from r to l
+        // if dir is 0(even)=moving from l to r
+        int rem=time%(n-1);
+        // this will tell about the position of pillow
+
+        if(div & 1){ //if dir is odd
             return n-rem;
+             //then piche move krte hue ans nikalo
         }
-        else {
-            return rem+1;
-        }
-        return rem;
+        return rem+1; //if even aage badho
     }
 };
