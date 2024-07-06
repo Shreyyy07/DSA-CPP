c892ce4165fc6d012bd9d3d7db5c608a7be24dee
shrey joshi <113532718+Shreyyy07@users.noreply.github.com>
2024-07-06 23:35:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (71.18%) - LeetSync

---
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int div=time/(n-1);
+        int rem=time%(n-1);
+
+        if(div%2==1){
+            return n-rem;
+        }
+        else {
+            return rem+1;
+        }
+        return rem;
+    }
+};
