f6de9c7e051ffe2d6a016178f16c8483c0db7983
shrey joshi <113532718+Shreyyy07@users.noreply.github.com>
2024-12-20 14:30:47 +0530

    Time: 0 ms (100.00%) | Memory: 96.1 MB (15.57%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -7,20 +7,20 @@ public:
         int i=0;
         int j=0;
 
-        while(i<n1 && j<n2){
+        while(i<n1 && j<n2)
             if(nums1[i]<nums2[j]){
                 nums3.push_back(nums1[i++]);
             }
             else{
                 nums3.push_back(nums2[j++]);
             }
-        }
-        while(i<n1){
+
+        while(i<n1)
            nums3.push_back(nums1[i++]);
-        }
-        while(j<n2){
+        
+        while(j<n2)
             nums3.push_back(nums2[j++]);
-        }
+        
         int n=n1+n2;
         if(n%2==1){
             return nums3[n/2];
