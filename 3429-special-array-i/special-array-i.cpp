35f7bcd8501bc51cdeadeff52172c115aaf6a8d4
shrey joshi <113532718+Shreyyy07@users.noreply.github.com>
2025-02-06 21:21:30 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (60.14%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -6,10 +6,9 @@ public:
         if(n==1){
             return true;
         }
-        for(int i=1; i<nums.size(); i++){
+        for(int i=0; i<nums.size()-1; i++){
                 int sum=0;
-                sum=nums[i-1]+nums[i];
-                if(sum%2==0){
+               if((nums[i]%2)==(nums[i+1]%2)){
                     return false;
                 }
         }
