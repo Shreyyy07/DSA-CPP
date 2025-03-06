655f3d024e1e1462b2ab9b1ccf57345189dc6130
shrey joshi <113532718+Shreyyy07@users.noreply.github.com>
2025-03-06 22:14:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (28.39%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -33,18 +33,11 @@ public:
         }
         }
 
-        while(list1!=NULL){
-             temp->next=list1;
-              temp=list1;
-            list1=list1->next;
-        }
-
-        while(list2!=NULL){
-             temp->next=list2;
-               temp=list2;
-            list2=list2->next;
-        }
-
+        if (list1 != NULL) {
+        temp->next = list1;
+    } else {
+        temp->next = list2;
+    }
         return ans->next;
     }
 };
