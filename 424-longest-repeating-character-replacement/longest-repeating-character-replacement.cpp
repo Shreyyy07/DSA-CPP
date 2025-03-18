2865909c8b49f77cb9dc3bff2ef285738cfaf1dc
shrey joshi <113532718+Shreyyy07@users.noreply.github.com>
2025-03-18 22:56:30 +0530

    Time: 7 ms (52.52%) | Memory: 10.9 MB (43.52%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,58 @@
+
+                             // BRUTE FORCE //
+
+// class Solution {
+// public:
+//     int characterReplacement(string s, int k) {
+
+//         int n=s.length();
+//         int maxlen=0;
+//         int maxfreq=0;
+//         int change;
+
+//         for(int i=0; i<s.length(); i++){
+//             int hash[26]={0};
+//             for(int j=i; j<s.length(); j++){
+//                 hash[s[j]-'A']++;
+
+//                 maxfreq=max(maxfreq,hash[s[j]-'A']);
+//                 change=(j-i+1)-maxfreq;
+//                 if(change<=k){
+//                     maxlen=max(maxlen,j-i+1);
+//                 }
+//                 else{
+//                     break;
+//                 }
+//             }
+//         }
+//         return maxlen;
+//     }
+// };
+
+                            // OPTIMIZE APPROACH //
+class Solution {
+public:
+    int characterReplacement(string s, int k) 
+    {
+
+        int l=0,r=0;
+        int maxlen=0;
+        int maxfreq=0;
+        int change;
+        unordered_map<char,int>mapp;
+
+        while(r<s.length()){
+            mapp[s[r]]++;
+            maxfreq=max(maxfreq,mapp[s[r]]);
+            change=(r-l+1)-maxfreq;
+
+            if(change>k){
+                mapp[s[l]]--;
+                l++;
+            }
+            maxlen=max(maxlen,r-l+1);
+            r++;
+        }
+        return maxlen;
+    }
+};
