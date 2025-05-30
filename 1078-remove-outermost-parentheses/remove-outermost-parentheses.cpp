9e40720e4472cfcae406f70a29190efc1ff79d29
Shrey Joshi <113532718+Shreyyy07@users.noreply.github.com>
2025-05-31 00:30:23 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (53.91%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+
+        string ans="";
+        int depth=0;
+
+        for(char ch:s){
+
+            if(ch=='('){
+                if(depth>0){
+                ans.push_back(ch);
+                }
+                depth++;
+            }
+            else if(ch==')'){
+                 depth--;
+               if(depth>0){
+                ans.push_back(ch);
+               }
+            }
+        }
+        return ans;
+    }
+};
