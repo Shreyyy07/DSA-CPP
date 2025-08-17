2783ec98c319d1a6619da436fa1fbfd0db1faeed
Shrey Joshi <113532718+Shreyyy07@users.noreply.github.com>
2025-08-17 18:40:53 +0530

    Time: 0 ms (100.00%) | Memory: 28 MB (98.76%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -23,39 +23,8 @@
 // };
 
 
+
                                       // BETTER APPROACH //
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        
-        int n=nums.size();
-        int cnt=0;
-        int ele;
-        map<int,int>mapp;
-
-        for(int i=0; i<n; i++){
-            mapp[nums[i]]++;
-        }
-        for(auto x:mapp){
-        if(x.second>n/2){
-            return x.first;
-        }
-        }
-        return -1;
-    }
-};                                      
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // class Solution {
@@ -65,22 +34,53 @@ public:
 //         int n=nums.size();
 //         int cnt=0;
 //         int ele;
+//         map<int,int>mapp;
 
 //         for(int i=0; i<n; i++){
-//             if(cnt==0){
-//                 cnt=1;
-//                 ele=nums[i];
-//             }
-//             else if(ele=nums[i]){
-//                 cnt++;
-//             }
-//             else{
-//                 cnt--;
-//             }
+//             mapp[nums[i]]++;
 //         }
-//         if(cnt>n/2){
-//             return ele;
+//         for(auto x:mapp){
+//         if(x.second>n/2){
+//             return x.first;
+//         }
 //         }
 //         return -1;
 //     }
-// };
+// };                                      
+
+                                    //  OPTIMIZED APPROACH  //
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int n=nums.size();
+        int cnt=0;
+        int ele;
+
+        for(int i=0; i<n; i++){
+            if(cnt==0){
+                cnt=1;
+                ele=nums[i];
+            }
+            else if(ele==nums[i]){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+        int cnt1=0;
+        for(int i=0; i<n; i++){
+            if(nums[i]==ele){
+                cnt1++;
+            }
+        }
+
+        if(cnt1>n/2){
+            return ele;
+        
+        }
+        return -1;
+    }
+};
