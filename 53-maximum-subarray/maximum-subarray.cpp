143659ad6f71043158b21fc39b41f5e3587b6e05
Shrey Joshi <113532718+Shreyyy07@users.noreply.github.com>
2025-08-17 16:16:40 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (16.63%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -8,7 +8,9 @@ public:
 
         for(int i=0; i<n; i++){
             sum+=nums[i];
-             maxi=max(maxi,sum);
+            if(sum>maxi){
+             maxi=sum;
+            }
            if(sum<0){
             sum=0;
            }
